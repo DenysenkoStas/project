505126f9416cf5f27b5068d5d01b9db7a77dc1ec
Stas <denysenko.stas@gmail.com>
2020-11-07 21:28:47 +0200

    Practice JS #2 - while + do cycles

--- js/script.js
+++ js/script.js
@@ -36,6 +36,42 @@ for (let i = 0; i < 2; i++) {
     }
 }
 
+// С помощью while
+// let i = 0;
+
+// while (i < 2) {
+//     i++;
+
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//           b = prompt("На сколько оцените его?");
+
+//     if (a != null && b != null && a != "" && b != "" && a.length <= 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log("done");
+//     } else {
+//         console.log("error");
+//         i--;
+//     }
+// }
+
+// С помощью do
+// let i = 0;
+
+// do {
+//     i++;
+
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//           b = prompt("На сколько оцените его?");
+
+//     if (a != null && b != null && a != "" && b != "" && a.length <= 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log("done");
+//     } else {
+//         console.log("error");
+//         i--;
+//     }
+// } while (i < 2);
+
 if (personalMovieDB.count < 10) {
     console.log("Просмотрено довольно мало фильмов");
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
