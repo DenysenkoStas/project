965924af23df42c6a17d52aa609910621730ac5f
Stas <denysenko.stas@gmail.com>
2020-11-08 10:31:42 +0200

    Lesson 20

--- js/test.js
+++ js/test.js
@@ -1,26 +1,43 @@
 "use strict";
 
-function first() {
-    // Do domething
-    setTimeout(function () {
-        console.log(1);
-    }, 500);
-}
+const options = {
+    name: "test",
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: "black",
+        bg: "red"
+    },
+    makeTest: function () {
+        console.log("Test");
+    }
+};
 
-function second() {
-    console.log(2);
-}
+options.makeTest();
 
-first();
-second();
+const {border, bg} = options.colors;
+console.log(border, bg);
 
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
+// console.log(Object.keys(options).length);
 
-function done() {
-    console.log("Я прошел этот урок");
-}
+// console.log(options.name);
 
-learnJS("Java Script", done);
+// delete options.name;
+
+// console.log(options);
+
+// for (let key in options) {
+//     if (typeof(options[key]) === "object") {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     }
+// }
+
+// let counter = 0;
+// for (let key in options) {
+//     counter++;
+// }
+// console.log(counter);
