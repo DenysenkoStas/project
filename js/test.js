8f41a1257665ce4e75fe790b2f2562b11655f582
Stas <denysenko.stas@gmail.com>
2020-11-28 15:06:43 +0200

    Lesson 29

--- js/test.js
+++ js/test.js
@@ -1,19 +1,52 @@
 "use strict";
 
-const box = document.getElementById("box");
-console.log(box);
+const box = document.getElementById("box"),
+      btns = document.getElementsByTagName("button"),
+      circles = document.getElementsByClassName("circle"),
+      wrapper = document.querySelector(".wrapper"),
+      hearts = wrapper.querySelectorAll(".heart"),
+      oneHeart = wrapper.querySelector(".heart");
 
-const btns =  document.getElementsByTagName("button")[1];
-console.log(btns);
+console.dir(box);
+// box.style.backgroundColor = "blue";
+// box.style.width = "500px";
 
-const circles = document.getElementsByClassName("circle");
-console.log(circles);
+box.style.cssText = "background-color: blue; width: 500px";
 
-const hearts = document.querySelectorAll(".heart");
+btns[1].style.borderRadius = "100%";
+circles[0].style.backgroundColor = "red";
+
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = "blue";
+// }
 
 hearts.forEach(item => {
-    console.log(item);
+    item.style.backgroundColor = "blue";
 });
 
-const oneHeart = document.querySelector(".heart");
-console.log(oneHeart);
+const div = document.createElement("div");
+// const text = document.createTextNode("Тут был я");
+
+div.classList.add("black");
+
+wrapper.append(div);
+// wrapper.appendChild(div); // Устаревший метод
+
+// wrapper.prepend(div);
+
+// hearts[1].before(div);
+// hearts[1].after(div);
+
+// wrapper.insertBefore(div, hearts[1]); // Устаревший метод
+
+// circles[1].remove();
+// wrapper.removeChild(hearts[1]); // Устаревший метод
+
+// hearts[1].replaceWith(circles[0]);
+// wrapper.replaceChild(circles[0], hearts[0]); // Устаревший метод
+
+div.innerHTML = "<h1>Hello World!</h1>";
+
+// div.textContent = "Hello";
+
+div.insertAdjacentHTML("beforebegin", "<h2>Hello</h2>");
