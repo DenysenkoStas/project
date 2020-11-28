0fd41c64957e8c692fd35abf5841b3d32e3bd340
Stas <denysenko.stas@gmail.com>
2020-11-28 13:43:07 +0200

    Lesson 28

--- js/test.js
+++ js/test.js
@@ -1,32 +1,19 @@
 "use strict";
 
-let x = 5;
-console.log(x++); // 5
+const box = document.getElementById("box");
+console.log(box);
 
-console.log([] + false - null + true); // "NaN"
+const btns =  document.getElementsByTagName("button")[1];
+console.log(btns);
 
-let y = 1;
-let x = y = 2;
-console.log(x); // 2
+const circles = document.getElementsByClassName("circle");
+console.log(circles);
 
-console.log([] + 1 + 2); // "12"
+const hearts = document.querySelectorAll(".heart");
 
-console.log("1" [0]); // "1"
+hearts.forEach(item => {
+    console.log(item);
+});
 
-console.log(2 && 1 && null && 0 && undefined); // null
-// И (&&) запинается на лжи (false)
-// ИЛИ (||) запинается на правде (true)
-
-console.log(!!( 1 && 2 ) == (1 && 2)); // false
-
-console.log(null || 2 && 3 || 4); // 3
-
-const a = [1, 2, 3],
-      b = [1, 2, 3];
-console.log (a == b); // false
-
-console.log(+"Infinity"); // Infinity - числоввой тип
-
-console.log("Ёжик" > "яблоко"); // false
-
-console.log(0 || "" || 2 || undefined || true || falsе); // 2
+const oneHeart = document.querySelector(".heart");
+console.log(oneHeart);
