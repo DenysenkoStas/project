9236d781092b41e854d6a7560f516b762a7b3ba0
Stas <denysenko.stas@gmail.com>
2021-01-30 00:20:10 +0200

    Lesson 81

---
+++ lesson81/script.js
@@ -0,0 +1,33 @@
+'use strict';
+// пример №1
+// console.log(1);
+
+// setTimeout(() => {
+//     console.log('timeout');
+// }, 2000);
+
+// setTimeout(() => {
+//     console.log('timeout');
+// }, 4000);
+
+// console.log(1);
+
+// пример №2
+// let k = 0;
+
+// function count() {
+//     for (let i = 0; i < 1e9; i++) {
+//         k++;
+//     }
+
+//     alert('done');
+// }
+
+// count();
+
+// пример №3
+setTimeout(() => {
+    console.log(0);
+}, 0);
+
+console.log(2);
